b25708feffd0deb851bd035ee1e0ad41c4640129
Vikeevika <36482897+Vikeevika@users.noreply.github.com>
2018-03-13 15:16:33 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -441,12 +441,32 @@
 			</div>
 		</div>
 	</div>
+	<div class="scrollup">
+        <img src="background/arrow.png" class="img-responsive center block" alt="Наверх">
+        </div>
 
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/bootstrap.js"></script>
+	<script>
+$(function() {
+		$('.scrollup').click(function() {
+			$("html, body").animate({
+			scrollTop:0
+			},1000);
+		})
+    })
+	$(window).scroll(function() {
+		if ($(this).scrollTop()>500) {
+			$('.scrollup').fadeIn();
+		}
+		else {
+			$('.scrollup').fadeOut();
+		}
+    });
+</script>
 </body>
 
 </html>
